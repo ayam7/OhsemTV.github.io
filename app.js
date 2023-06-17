cf96bf20c4db8dd32095ac9aaad7074779203a2f
ayam7 <84507167+ayam7@users.noreply.github.com>
2023-06-17 17:07:48 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+function toggle() {
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+
+function toggle2() {
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const pop2 = document.getElementById('res-popup');
+    pop2.classList.toggle('active');
+}
+
+function toggle3() {
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const pop3 = document.getElementById('res-popup2');
+    pop3.classList.toggle('active');
+}
+
+function toggle4() {
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const pop4 = document.getElementById('pre-popup');
+    pop4.classList.toggle('active');
+
+}
+
+function thanku() {
+    alert('Thank you for submitting!');
+}
+
+function thanku2() {
+    alert('Thank you for Buying!');
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const pop4 = document.getElementById('pre-popup');
+    pop4.classList.toggle('active');
+}
